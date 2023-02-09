bea7c799374af7722eb2a7426fbb8ae9e65819e9
Mutsa01 <mutsa78@gmail.com>
2023-02-09 19:45:41 +0000

    created bottom nav menu

--- hotel-chrome-extension/src/App.tsx
+++ hotel-chrome-extension/src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './new_logo.svg';
 import exit from './grey_exit_icon.svg';
+import search from './icons/search_icon.svg';
+import house from'./icons/house_icon1.svg';
+import plus from './icons/plus_grey.svg';
 import './App.css';
 
 function App() {
@@ -18,7 +21,13 @@ function App() {
           <img src={exit} className='exit-icon' alt='exit icon'/>
         </div>
       </header>
-      
+      <body>
+        <div className="bottom-nav-container">
+          <img src={search} className="bottom-nav-item" alt="search icon" />
+          <img src={house} className="bottom-nav-item" alt="home icon" />
+          <img src={plus} className="bottom-nav-item" alt="plus icon" />
+        </div>
+      </body>
     </div>
   );
 }
