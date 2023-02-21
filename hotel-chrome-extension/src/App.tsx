f1e8e8fd0794c9e5ab55d424099abe770eff9087
Mutsa01 <mutsa78@gmail.com>
2023-02-21 14:40:32 +0000

    retrieving header info test

--- hotel-chrome-extension/src/App.tsx
+++ hotel-chrome-extension/src/App.tsx
@@ -5,11 +5,42 @@ import search from './icons/search_icon.svg';
 import house from'./icons/house_icon1.svg';
 import plus from './icons/plus_grey.svg';
 import './App.css';
+import { DOMMessage, DOMMessageResponse } from './types';
 
 function App() {
   const handleExitClick = () => {
     window.close();
   };
+
+  const [title, setTitle] = React.useState('');
+  const [headlines, setHeadlines] = React.useState<string[]>([]);
+
+  React.useEffect(() => {
+    /**
+     * We can't use "chrome.runtime.sendMessage" for sending messages from React.
+     * For sending messages from React we need to specify which tab to send it to.
+     */
+    chrome.tabs && chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, tabs => {
+      /**
+       * Sends a single message to the content script(s) in the specified tab,
+       * with an optional callback to run when a response is sent back.
+       *
+       * The runtime.onMessage event is fired in each content script running
+       * in the specified tab for the current extension.
+       */
+      chrome.tabs.sendMessage(
+        tabs[0].id || 0,
+        { type: 'GET_DOM' } as DOMMessage,
+        (response: DOMMessageResponse) => {
+          setTitle(response.title);
+          setHeadlines(response.headlines);
+        });
+    });
+  });
+
   return (
     <div className="App">
       <header className="App-header">
@@ -26,6 +57,9 @@ function App() {
           <img src={search} className="bottom-nav-item" alt="search icon" />
           <img src={house} className="bottom-nav-item" alt="home icon" />
           <img src={plus} className="bottom-nav-item" alt="plus icon" />
+          <span className={`SEOValidationFieldStatus ${headlines.length !== 1 ? 'Error' : 'Ok'}`}>
+              {headlines.length}
+            </span>
         </div>
       </body>
     </div>
